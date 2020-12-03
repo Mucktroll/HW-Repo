ad64ae856efb19ad0d99d965ad32699dca378338
Mucktroll <58229340+Mucktroll@users.noreply.github.com>
2020-12-03 13:49:42 +0200

    Add files via upload

---
+++ ib-cs-vending-machine-master/Product.java
@@ -0,0 +1,36 @@
+// Copyright (C) 2020 Jarmo Hurri
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+enum Product
+{
+  Beverage (120), Apple (65), Banana (50), Chocolate(100);// product types with their prices in cents
+
+  private int priceCents;
+
+  Product (int priceCents)
+  {
+    this.priceCents = priceCents;
+  }
+
+  public int getPriceCents ()
+  {
+    return priceCents;
+  }
+  
+  public String toString ()
+  {
+    return (name ()) + " (" + priceCents + " cents)";
+  }
+}
