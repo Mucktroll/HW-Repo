6f4c0da4ee1d8355f6d67ee8a47c3b5829805ab4
Mucktroll <58229340+Mucktroll@users.noreply.github.com>
2021-01-13 14:55:34 +0200

    Add files via upload

--- HW3/src/com/company/Main.java
+++ HW3/src/com/company/Main.java
@@ -6,7 +6,9 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println(TeleNumsRecursive(10));
-        
+
+        System.out.println(TeleNumsIterative(10));
+
     }
 
     public static int TeleNumsRecursive (int n)
@@ -14,6 +16,28 @@ public class Main
         if (n >= 2)
         {
             return TeleNumsRecursive(n - 1) + ((n - 1)*TeleNumsRecursive(n - 2));
+        }
+
+        return 1;
+    }
+
+    public static int TeleNumsIterative (int n)
+    {
+        if (n >= 2)
+        {
+            int nCurrent = 1;
+            int cur = 1;
+            int prev = 1;
+
+            while (nCurrent < n)
+            {
+                int next = cur + nCurrent * prev;
+                prev = cur;
+                cur = next;
+                nCurrent++;
+            }
+
+            return cur;
         }
 
         return 1;
